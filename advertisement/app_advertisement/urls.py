5d656432325caca38e5c21fb1f5813c40d0434f0
aiwobonappetit <toscarina@bk.ru>
2023-08-18 10:22:41 +0300

    Занятие 5 / Проделана домашняя работа, все ссылки в шаблонах изменены при помощи шаблон-тега {% url <> %}

--- advertisement/app_advertisement/urls.py
+++ advertisement/app_advertisement/urls.py
@@ -4,4 +4,8 @@ from .views import *
 urlpatterns = [
     path("", index, name="main-page"),
     path("top-sellers/", top_sellers, name="top-sellers"),
+    path("advertisement-post/", advertisement_post, name="advertisement-post"),
+    path("register/", register, name="register"),
+    path("login/", login, name="login"),
+    path("profile/", profile, name="profile"),
 ]
--- advertisement/app_advertisement/views.py
+++ advertisement/app_advertisement/views.py
@@ -6,3 +6,15 @@ def index(request):
 
 def top_sellers(request):
     return render(request, "top-sellers.html")
+
+def advertisement_post(request):
+    return render(request, "advertisement-post.html")
+
+def register(request):
+    return render(request, "register.html")
+
+def login(request):
+    return render(request, "login.html")
+
+def profile(request):
+    return render(request, "profile.html")
