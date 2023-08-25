b55889a9597b9e1fab60dfc1dda0829272c35442
aiwobonappetit <toscarina@bk.ru>
2023-08-25 17:11:33 +0300

    Занятие 7 / Проделана работа на лекции + домашняя

--- advertisement/advertisement/settings.py
+++ advertisement/advertisement/settings.py
@@ -19,12 +19,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # core
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # my apps
     'app_advertisement',
     'app_lesson_4',
 ]
@@ -66,7 +68,7 @@ WSGI_APPLICATION = 'advertisement.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': Path.joinpath(BASE_DIR, 'db.sqlite3'),
     }
 }
 
