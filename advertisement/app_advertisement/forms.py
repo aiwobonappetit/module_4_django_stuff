d1c6805981740d8d16cea9037476c95423724dfb
aiwobonappetit <toscarina@bk.ru>
2023-08-27 02:41:02 +0300

    Занятие 10 / +Д/З

--- advertisement/app_advertisement/forms.py
+++ advertisement/app_advertisement/forms.py
@@ -1,23 +1,22 @@
 from django import forms
+from django.forms import ModelForm
+from .models import Advertisement
+from django.core.exceptions import ValidationError
 
-class AdvertisementForm(forms.Form):
-    title = forms.CharField(
-        max_length=60,
-        widget=forms.TextInput(attrs={"class" : "form-control-lg"})
-    )
+class AdvertisementForm(ModelForm):
+    class Meta:
+        model = Advertisement
+        fields = ["title", "description", "price", "auction", "image"]
+        widgets = {
+            "title" : forms.TextInput(attrs={"class": "form-control form-control-lg"}),
+            "description" : forms.Textarea(attrs={"class": "form-control form-control-lg"}),
+            "price" : forms.NumberInput(attrs={"class": "form-control form-control-lg"}),
+            "auction" : forms.CheckboxInput(attrs={"class": "form-check-input"}),
+            "image" : forms.FileInput(attrs={"class": "form-control form-control-lg"})
+        }
 
-    description = forms.CharField(
-        widget=forms.Textarea(attrs={"class" : "form-control-lg"})
-    )
-
-    price = forms.DecimalField(
-        widget=forms.NumberInput(attrs={"class" : "form-control-lg"})
-    )
-
-    auction = forms.BooleanField(
-        required=False,
-        widget=forms.CheckboxInput(attrs={"class" : "form-check-input"})
-                                 )
-    image = forms.ImageField(
-        widget=forms.FileInput(attrs={"class" : "form-control-lg"})
-    )
+    def valid_title(self):
+        title = self.cleaned_data.get("title")
+        if title and title.startswith("?"):
+            raise ValidationError("Название объявления не может начинаться с '?'.")
+        return title
